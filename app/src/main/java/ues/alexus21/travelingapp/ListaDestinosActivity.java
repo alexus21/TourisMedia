97fb970da5f91205d489bf711f914ddf5c454436
Alex Ulloa <US21003@ues.edu.sv>
2024-06-01 16:52:37 -0600

    Nombre de usuario en la parte superior

--- app/src/main/java/ues/alexus21/travelingapp/ListaDestinosActivity.java
+++ app/src/main/java/ues/alexus21/travelingapp/ListaDestinosActivity.java
@@ -1,11 +1,27 @@
 package ues.alexus21.travelingapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
+
+import ues.alexus21.travelingapp.localstorage.ILocalUserDAO;
+
 public class ListaDestinosActivity extends AppCompatActivity {
+    TextView lblUsuarioLogeado;
+    private ILocalUserDAO localUserDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +29,55 @@ public class ListaDestinosActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lista_destinos);
 
+        lblUsuarioLogeado = findViewById(R.id.lblUsuarioLogeado);
+
+        // Llama a setLoggedUserName para establecer el nombre de usuario
+        setLoggedUserName();
+    }
+
+    void setLoggedUserName() {
+        // Obtener referencia a la base de datos de Firebase
+        DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference("users");
+        localUserDAO = DatabaseSingleton.getDatabase(this).localUserDAO();
+
+        // Leer el ID de Firebase almacenado en la base de datos
+        databaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    String idFirebase = userSnapshot.child("id").getValue(String.class);
+                    if (idFirebase != null) {
+                        Log.d("ID de Firebase", idFirebase);
+
+                        // Obtener el ID local
+                        String idLocal = localUserDAO.getUserId();
+                        if (idLocal != null) {
+                            Log.d("ID Local", idLocal);
+
+                            String email = userSnapshot.child("email").getValue(String.class);
+                            // Remover todo lo que vaya desde @ (la @ incluida)
+                            String name = email.substring(0, email.indexOf('@'));
+
+                            System.out.println(name);
+
+                            // Comparar los IDs
+                            if (idFirebase.equals(idLocal)) {
+                                lblUsuarioLogeado.setText(name);
+                            } else {
+                                lblUsuarioLogeado.setText("Invitado");
+                            }
+                        }
+                    } else {
+                        Log.d("ID de Firebase", "El ID de Firebase es null");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("DatabaseError", "Error al leer el ID de Firebase", databaseError.toException());
+            }
+        });
     }
 }
--- app/src/main/java/ues/alexus21/travelingapp/MainActivity.java
+++ app/src/main/java/ues/alexus21/travelingapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -28,11 +29,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         // Inicializar el DAO
         localUserDAO = DatabaseSingleton.getDatabase(this).localUserDAO();
@@ -71,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.e(TAG, "Error al leer el ID de Firebase", databaseError.toException());
             }
         });
