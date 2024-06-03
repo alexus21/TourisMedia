8759ce94cb57fad9fd878d9e0d12e5072cf71daf
Alex Ulloa <US21003@ues.edu.sv>
2024-06-03 16:12:16 -0600

    quitada barra de búsqueda

---
+++ app/src/main/java/ues/alexus21/travelingapp/activities/PlaceReviewActivity.java
@@ -0,0 +1,19 @@
+package ues.alexus21.travelingapp.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ues.alexus21.travelingapp.R;
+
+public class PlaceReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_place_review);
+
+    }
+}
